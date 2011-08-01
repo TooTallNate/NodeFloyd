cc400d66286233c6c52bb16ef2973f00296d8444
Nathan Rajlich <nathan@tootallnate.net>
2011-07-31 23:33:58 -0700

    Comma-first is the way, my friends

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("colors");
-var connect = require('connect');
-var throttle = require('throttle');
-var encoder = require('./encoder');
+var connect = require('connect')
+  , throttle = require('throttle')
+  , encoder = require('./encoder')
 
 process.title = "NodeFloyd";
 
@@ -10,14 +10,14 @@ var stdin = process.openStdin();
 encoder.stdin = stdin;
 
 // Stdin is expecting raw PCM data of the format:
-var SAMPLE_SIZE = 16;   // 16-bit samples, Little-Endian, Signed
-var CHANNELS = 2;       // 2 channels (left and right)
-var SAMPLE_RATE = 44100;// 44,100 Hz sample rate.
+var SAMPLE_SIZE = 16    // 16-bit samples, Little-Endian, Signed
+  , CHANNELS = 2        // 2 channels (left and right)
+  , SAMPLE_RATE = 44100 // 44,100 Hz sample rate.
 
 // If we're getting raw PCM data as expected, calculate the number of bytes
 // that need to be read for `1 Second` of audio data.
-var BLOCK_ALIGN = SAMPLE_SIZE / 8 * CHANNELS; // Number of 'Bytes per Sample'
-var BYTES_PER_SECOND = SAMPLE_RATE * BLOCK_ALIGN;
+var BLOCK_ALIGN = SAMPLE_SIZE / 8 * CHANNELS // Number of 'Bytes per Sample'
+  , BYTES_PER_SECOND = SAMPLE_RATE * BLOCK_ALIGN
 
 
 // Throttle stdin based on the calculated BYTES_PER_SECOND value
@@ -36,7 +36,7 @@ function onStdinData(chunk) {
   while (currentBocSize() > bocSize) {
     removed += bocData.shift().length;
   }
-  
+
   // If we've removed a number of bytes that isn't a multiple of BLOCK_ALIGN,
   // then we'd be left with a partial audio sample, which at best case reverses
   // the audio channels, and at worst makes the bytes 16-bit ints be offset by 1,
@@ -52,9 +52,9 @@ function onStdinData(chunk) {
   }
 }
 function currentBocSize() {
-  var size = 0;
-  var i=0
-  var l=bocData.length;
+  var size = 0
+    , i=0
+    , l=bocData.length
   for (; i<l; i++) {
     size += bocData[i].length;
   }
@@ -63,8 +63,8 @@ function currentBocSize() {
 
 
 var name = "TooTallNate's Pink Floyd Collection"
+  , metaint = 8192
 encoder.name = name;
-var metaint = 8192;
 encoder.metaint = metaint;
 // Array of HttpServerResponse objects that are listening clients.
 encoder.clients = clients = [];
@@ -78,9 +78,9 @@ encoder.maxClients = maxClients = 15;
 
 encoder.metadata = metadata = {};
 encoder.currentTrack = currentTrack = "unknown";
-var currentTrackStartTime;
-var duration;
-var dId;
+var currentTrackStartTime
+  , duration
+  , dId
 // Using 'stdin' just because it's a convient "main" EventEmitter to piggy back from
 stdin.on("metadata", function(metadataObj) {
   encoder.metadata = metadata = metadataObj;
@@ -94,72 +94,74 @@ stdin.on("metadata", function(metadataObj) {
 });
 
 // Now we create the HTTP server.
-var server = connect.createServer(
-  connect.logger(),
-  encoder.Encoder('/stream.mp3', 'audio/mpeg', "lame", [
-    "-S", // Operate silently (nothing to stderr)
-    "-r", // Input is raw PCM
-    "-s", SAMPLE_RATE / 1000, // Input sampling rate: 44,100
-    "-", // Input from stdin
-    "-" // Output to stderr
-  ]),
-  encoder.Encoder('/stream.ogg', 'application/ogg', "oggenc", [
-    "--silent", // Operate silently (nothing to stderr)
-    "-r", // Raw input
-    "--raw-rate=" + SAMPLE_RATE, // Raw input rate: 44100
-    "-" // Input from stdin, Output to stderr
-  ]),
-  // TODO: Add 'faac' for AAC encoding support!!
-  encoder.Encoder('/stream.aacp', 'audio/aacp', process.env.HOME + '/aacplusenc/aacplusenc', [
-    // No args for now
-  ]),
-  function(req, res, next) {
+var server = connect();
+server.use(connect.logger('dev'));
+server.use(encoder.Encoder('/stream.mp3', 'audio/mpeg', "lame", [
+    "-S" // Operate silently (nothing to stderr)
+  , "-r" // Input is raw PCM
+  , "-s", SAMPLE_RATE / 1000 // Input sampling rate: 44,100
+  , "-"// Input from stdin
+  , "-" // Output to stderr
+]))
+server.use(encoder.Encoder('/stream.ogg', 'application/ogg', "oggenc", [
+    "--silent" // Operate silently (nothing to stderr)
+  , "-r" // Raw input
+  , "--raw-rate=" + SAMPLE_RATE // Raw input rate: 44100
+  , "-" // Input from stdin, Output to stderr
+]))
+// TODO: Add 'faac' for AAC encoding support!!
+server.use(encoder.Encoder('/stream.aacp', 'audio/aacp', process.env.HOME + '/aacplusenc/aacplusenc', [
+  // No args for now
+]))
 
-    // If "/metadata" is requested, then hold of on sending any response, but
-    // request the `icecast.ReadStream` instance to notify the request of the next
-    // 'metadata' event.
-    if (req.url == "/metadata") {
-      if (req.method == "POST") {
+// This little middleware provdies an HTTP endpoint for getting/settings the
+// metadata of the currently playing track.
+server.use(function(req, res, next) {
 
-        var auth = req.headers.authorization;
-        var ct = req.headers['x-title'];
-        var returnCode = 401;
-        if (ct && auth && auth.substring(0, 6) == "Basic " && Buffer(auth.substring(6), 'base64').toString('ascii') == 'node:rules') {
-          stdin.emit('metadata', {
-            title: ct,
-            artist: req.headers['x-artist'],
-            album: req.headers['x-album'],
-            duration: req.headers['x-duration']
-          });
-          returnCode = 200;
-        }
-        res.writeHead(returnCode);
-        res.end();        
+  // If "/metadata" is requested, then hold of on sending any response, but
+  // request the `icecast.ReadStream` instance to notify the request of the next
+  // 'metadata' event.
+  if (req.url == "/metadata") {
+    if (req.method == "POST") {
 
-      } else if (req.headers['x-current-track']) {
+      var auth = req.headers.authorization
+        , ct = req.headers['x-title']
+        , returnCode = 401
+      if (ct && auth && auth.substring(0, 6) == "Basic " && Buffer(auth.substring(6), 'base64').toString('ascii') == 'node:rules') {
+        stdin.emit('metadata', {
+            title: ct
+          , artist: req.headers['x-artist']
+          , album: req.headers['x-album']
+          , duration: req.headers['x-duration']
+        });
+        returnCode = 200;
+      }
+      res.writeHead(returnCode);
+      res.end();
+
+    } else if (req.headers['x-current-track']) {
+      var metadataJson = JSON.stringify(metadata);
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(metadataJson)
+      });
+      res.end(metadataJson);
+    } else {
+      req.connection.setTimeout(60 * 60 * 1000); // One-hour timeouts
+      stdin.once("metadata", function(metadata) {
         var metadataJson = JSON.stringify(metadata);
         res.writeHead(200, {
-          'Content-Type': 'application/json',
-          'Content-Length': Buffer.byteLength(metadataJson)
+            'Content-Type': 'application/json'
+          , 'Content-Length': Buffer.byteLength(metadataJson)
         });
         res.end(metadataJson);
-      } else {
-        req.connection.setTimeout(60 * 60 * 1000); // One-hour timeouts
-        stdin.once("metadata", function(metadata) {
-          var metadataJson = JSON.stringify(metadata);
-          res.writeHead(200, {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(metadataJson)
-          });
-          res.end(metadataJson);
-        });
-      }
-    } else {
-      next();
+      });
     }
-  },
-  connect.static(__dirname + "/www")
-);
+  } else {
+    next();
+  }
+})
+server.use(connect.static(__dirname + "/www"))
 server.listen(5555);
 console.log(("HTTP Icecast server listening at: ".bold + "http://*:" + server.address().port).cyan);
 
